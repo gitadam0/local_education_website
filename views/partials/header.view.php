16a57c96cb26a827c54773a4a53abd31303aa219
ADAM <adm.mdd@gmail.com>
2023-04-24 15:10:54 +0100

    navbar and simple card deck

---
+++ views/partials/header.view.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body  style="height:1000px;">
+    <style >
+        
+        .myrow{
+            background-color :red;
+            height:300px;
+        }
+    </style>
+
+     
+
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3 py-3 px-5">
+  <a class="navbar-brand mb-0 h1" href="#">Ofppt-Learn</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Courses </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Leaderboard</a>
+      </li>
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+      <li class="nav-item">
+        <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Code Bits</a>
+      </li>
+    </ul>
+    
+      
+      <button class="btn btn-outline-success my-2 my-sm-0" >login</button>
+    
+  </div>
+</nav>
+
+
+
+
+
+
+<!-- <div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div> -->
